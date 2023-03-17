39befe0b069faf6960d832b798b3bb289bfcb099
SimoxG7 <simone.gioe@studenti.unimi.it>
2023-03-17 15:01:48 +0100

    added some leetcodes

---
+++ LeetCode (Dailys and from Problemset)/49. Group Anagrams/Solution.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+
+    Map<String, List<String>> map = new HashMap<>();
+
+    for (String s : strs) {
+
+      char[] valArr = s.toCharArray();
+      Arrays.sort(valArr);
+      String key = new String(valArr);
+
+      List<String> ll = map.getOrDefault(key, new ArrayList<String>());
+      ll.add(s);
+      map.put(key, ll);
+
+    }
+
+    List<List<String>> ans = new ArrayList<>();
+    for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+      ans.add(entry.getValue());
+    }
+
+    return ans;
+  }
+}
