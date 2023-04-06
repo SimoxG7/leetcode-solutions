0f669c6d21e53c9cafeac84a9acf3aa296a4b344
SimoxG7 <simone.gioe@studenti.unimi.it>
2023-04-06 16:44:39 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/1254. Number of Closed Islands/Solution.java
@@ -0,0 +1,28 @@
+class Solution {
+  public int closedIsland(int[][] grid) {
+    int cont = 0;
+    int n = grid.length, m = grid[0].length;
+    boolean vis[][] = new boolean[n][m];
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (grid[i][j] == 0) {
+          cont += dfs(i, j, grid, vis, n, m);
+        }
+      }
+    }
+    return cont;
+  }
+
+  public static int dfs(int i, int j, int a[][], boolean vis[][], int n, int m) {
+    if (i >= n || j >= m || i < 0 || j < 0)
+      return 0;
+    if (a[i][j] == 1)
+      return 1;
+    a[i][j] = 1;
+    int up = dfs(i - 1, j, a, vis, n, m);
+    int down = dfs(i + 1, j, a, vis, n, m);
+    int left = dfs(i, j - 1, a, vis, n, m);
+    int right = dfs(i, j + 1, a, vis, n, m);
+    return up & down & left & right;
+  }
+}
