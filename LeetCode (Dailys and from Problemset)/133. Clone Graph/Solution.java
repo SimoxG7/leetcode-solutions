b0923b5bfb1d94ce2959fb6cd0be942858e9517e
SimoxG7 <simone.gioe@studenti.unimi.it>
2023-04-08 16:50:51 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/133. Clone Graph/Solution.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+  class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+      val = 0;
+      neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+      val = _val;
+      neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+      val = _val;
+      neighbors = _neighbors;
+    }
+  }
+
+  public Node cloneGraph(Node node) {
+    if (node == null) {
+      return null;
+    }
+    Map<Node, Node> visited = new HashMap<>();
+    return cloneGraphHelper(node, visited);
+  }
+
+  private Node cloneGraphHelper(Node node, Map<Node, Node> visited) {
+    Node copy = new Node(node.val);
+    visited.put(node, copy);
+    for (Node neighbor : node.neighbors) {
+      if (visited.containsKey(neighbor)) {
+        copy.neighbors.add(visited.get(neighbor));
+      } else {
+        Node neighborCopy = cloneGraphHelper(neighbor, visited);
+        copy.neighbors.add(neighborCopy);
+      }
+    }
+    return copy;
+  }
+}
