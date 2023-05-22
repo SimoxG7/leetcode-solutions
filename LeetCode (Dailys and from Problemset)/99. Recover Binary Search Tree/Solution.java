b9871869503e38510e5d345d49a3ab1547cd0595
Simox <simox.gio1d@gmail.com>
2023-05-22 15:30:21 +0200

    added another leetcode

---
+++ LeetCode (Dailys and from Problemset)/99. Recover Binary Search Tree/Solution.java
@@ -0,0 +1,46 @@
+class Solution {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  TreeNode firstElement = null;
+  TreeNode secondElement = null;
+  TreeNode prevElement = new TreeNode(Integer.MIN_VALUE);
+
+  public void recoverTree(TreeNode root) {
+    traverse(root);
+    int temp = firstElement.val;
+    firstElement.val = secondElement.val;
+    secondElement.val = temp;
+  }
+
+  private void traverse(TreeNode root) {
+    if (root == null)
+      return;
+    traverse(root.left);
+    if (firstElement == null && prevElement.val > root.val) {
+      firstElement = prevElement;
+    }
+    if (firstElement != null && prevElement.val > root.val) {
+      secondElement = root;
+    }
+    prevElement = root;
+    traverse(root.right);
+  }
+}
