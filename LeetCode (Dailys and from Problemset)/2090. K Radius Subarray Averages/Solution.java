093ad2c0467b30ffd0872e3b63ae765a0f92b779
Simox <simox.gio1d@gmail.com>
2023-06-20 17:29:25 +0200

    added temp solution

---
+++ LeetCode (Dailys and from Problemset)/2090. K Radius Subarray Averages/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+  public int[] getAverages(int[] nums, int k) {
+    int n = nums.length;
+    int[] res = new int[n];
+    int range = 2 * k + 1;
+    if (k != 0 && range >= n) {
+      Arrays.fill(res, 0, res.length, -1);
+      return res;
+    } 
+    long[] op = new long[n];
+    for (int i = 0; i < range; i++) {
+      op[k] += nums[i];
+    }
+    for (int i = k + 1; i < n - k; i++) {
+      op[i] = op[i - 1] + nums[i + k] - nums[i - k - 1];
+    }
+    for (int i = 0; i < k; i++) {
+      res[i] = -1;
+      res[n - i - 1] = -1;
+    }
+    for (int i = k; i < n - k; i++) {
+      System.out.println(i + ": " + op[i]);
+      res[i] = (int) op[i]/range;
+    }
+    return res;
+  }
+}
+
