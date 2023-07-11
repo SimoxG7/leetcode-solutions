5ec099949c96bfee6b06c823aefc764cec6b7bfa
Simox <simox.gio1d@gmail.com>
2023-07-11 17:15:10 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/863. All Nodes Distance K in Binary Tree/Solution.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+  
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+      val = x;
+    }
+  }
+
+  void printKdown(TreeNode root, int depth, TreeNode block, List<Integer> ans) {
+    if (root == null || depth < 0 || root == block) {
+      return;
+    }
+    if (depth == 0) {
+      ans.add(root.val);
+      return;
+    }
+    printKdown(root.left, depth - 1, block, ans);
+    printKdown(root.right, depth - 1, block, ans);
+  }
+
+  int getNodetoRootPath(TreeNode root, TreeNode target, int k, List<Integer> ans) {
+    if (root == null) {
+      return -1;
+    }
+    if (root == target) {
+      printKdown(root, k, null, ans);
+      return 1;
+    }
+    int left = getNodetoRootPath(root.left, target, k, ans);
+    if (left > 0) {
+      printKdown(root, k - left, root.left, ans);
+      return left + 1;
+    }
+    int right = getNodetoRootPath(root.right, target, k, ans);
+    if (right > 0) {
+      printKdown(root, k - right, root.right, ans);
+      return right + 1;
+    }
+    return -1;
+  }
+
+  public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    List<Integer> ans = new ArrayList<>();
+    getNodetoRootPath(root, target, k, ans);
+    return ans;
+  }
+}
