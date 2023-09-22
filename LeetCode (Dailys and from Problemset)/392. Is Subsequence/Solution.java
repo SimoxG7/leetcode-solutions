d89766ff4ec6f907c4bf03a1d71fa9c2fcaba3db
Simox <simox.gio1d@gmail.com>
2023-09-22 15:19:27 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/392. Is Subsequence/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+  public boolean isSubsequence(String s, String t) {
+    if (s.isBlank()) return true;
+    int index = 0;
+    for (int i = 0; i < t.length(); i++) {
+      if (t.charAt(i) == s.charAt(index)) {
+        index++;
+        if (index == s.length()) return true;
+      }
+    }
+    return index == s.length();
+  }
+}
