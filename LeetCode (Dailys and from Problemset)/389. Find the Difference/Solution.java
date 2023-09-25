44f0add2f9434b631fa2a9e5b67be6d054b57fc0
Simox <simox.gio1d@gmail.com>
2023-09-25 11:19:07 +0200

    added a couple more solutions

--- LeetCode (Dailys and from Problemset)/389. Find the Difference/Solution.java
+++ LeetCode (Dailys and from Problemset)/389. Find the Difference/Solution.java
@@ -7,8 +7,29 @@ class Solution {
     }
     occurrences[t.charAt(s.length()) - 'a']++;
     for (int i = 0; i < occurrences.length; i++) {
-      if (occurrences[i] % 2 != 0) return (char) ('a' + i);
+      if (occurrences[i] % 2 != 0)
+        return (char) ('a' + i);
     }
     return 0;
   }
+}
+
+class Solution2 {
+  public char findTheDifference(String s, String t) {
+    char c = 0;
+    for (char cs : s.toCharArray())
+      c ^= cs;
+    for (char ct : t.toCharArray())
+      c ^= ct;
+    return c;
+  }
+}
+
+class Solution3 {
+  public char findTheDifference(String s, String t) {
+    int ans = 0;
+    for (char c : t.toCharArray()) ans += c;
+    for (char c : s.toCharArray()) ans -= c;
+    return (char) ans;
+  } 
 }
