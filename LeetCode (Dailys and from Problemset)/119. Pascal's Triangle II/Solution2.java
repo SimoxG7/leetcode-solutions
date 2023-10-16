a01e737e7c608ed0768fb3169439a38701fee273
SimoxG7 <simone.gioe@studenti.unimi.it>
2023-10-16 14:02:58 +0200

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/119. Pascal's Triangle II/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution2 {
+  public List<Integer> getRow(int rowIndex) {
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> prev = new ArrayList<>();
+    prev.add(1);
+    res.add(prev);
+    for (int i = 1; i <= rowIndex; i++) {
+      List<Integer> curr = new ArrayList<>();
+      curr.add(1);
+      for (int j = 0; j < prev.size() - 1; j++) {
+        curr.add(prev.get(j) + prev.get(j + 1));
+      }
+      curr.add(1);
+      prev = curr;
+      res.add(prev);
+    }
+    return res.get(rowIndex);
+  }
+}
