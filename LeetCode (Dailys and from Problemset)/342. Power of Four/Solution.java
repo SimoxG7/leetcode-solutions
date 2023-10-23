c205950d03a46f8ef67def8e2f69a9653bdd24d5
Simox <simox.gio1d@gmail.com>
2023-10-23 12:28:28 +0200

    even better solution with bit shift

--- LeetCode (Dailys and from Problemset)/342. Power of Four/Solution.java
+++ LeetCode (Dailys and from Problemset)/342. Power of Four/Solution.java
@@ -2,7 +2,8 @@ class Solution {
   public boolean isPowerOfFour(int n) {
     while (n > 1) {
       if (n % 4 != 0) return false;
-      n /= 4;
+      // n /= 4;
+      n >>= 2;
     }
     return n == 1;
   }
