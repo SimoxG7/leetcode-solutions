d266551b1b4c37df681327a944c85c8ac3e510eb
SimoxG7 <simone.gioe@studenti.unimi.it>
2023-12-09 19:46:34 +0100

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/94. Binary Tree Inorder Traversal/Solution2.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution2 {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public List<Integer> inorderTraversal(TreeNode root) {
+    List<Integer> res = new ArrayList<>();
+    visit(root, res);
+    return res;
+  }
+
+  private void visit(TreeNode root, List<Integer> nodes) {
+    if (root == null) return;
+    visit(root.left, nodes);
+    nodes.add(root.val);
+    visit(root.right, nodes);
+  }
+}
