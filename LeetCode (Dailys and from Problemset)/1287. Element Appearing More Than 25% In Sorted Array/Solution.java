7f6d2ef07466d4fc262ab1b8204eac21262c25a3
Simox <simox.gio1d@gmail.com>
2023-12-11 14:01:17 +0100

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/1287. Element Appearing More Than 25% In Sorted Array/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+  public int findSpecialInteger(int[] arr) {
+    if (arr.length < 2) {
+      return arr[0];
+    }
+    int result = 0;
+    int percent = (int) (arr.length * 0.25);
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i : arr) {
+      map.put(i, map.getOrDefault(i, 0) + 1);
+    }
+
+    for (int key : map.keySet()) {
+      if (map.get(key) > percent) {
+        result = key;
+      }
+    }
+
+    return result;
+  }
+}
