9bc9bb88bdb2605bc114aa3406cacf57ee7c002a
Simox <simox.gio1d@gmail.com>
2024-01-03 12:18:29 +0100

    improved a bit

--- LeetCode (Dailys and from Problemset)/2125. Number of Laser Beams in a Bank/Solution.java
+++ LeetCode (Dailys and from Problemset)/2125. Number of Laser Beams in a Bank/Solution.java
@@ -1,20 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.List;
 
 class Solution {
   public int numberOfBeams(String[] bank) {
-    List<Integer> acc = new ArrayList<>();
+    // List<Integer> acc = new ArrayList<>();
+    int[] acc = new int[bank.length];
+    int size = 0;
     for (int i = 0; i < bank.length; i++) {
-      String curr = bank[i];
+      char[] curr = bank[i].toCharArray();
       int cont = 0;
-      for (int j = 0; j < curr.length(); j++) {
-        if (curr.charAt(j) == '1') cont++; 
+      for (int j = 0; j < curr.length; j++) {
+        if (curr[j] == '1') cont++; 
       }
-      if (cont > 0) acc.add(cont);
+      // if (cont > 0) acc.add(cont);
+      if (cont > 0) acc[size++] = cont;
     }
     int res = 0;
-    for (int i = 1; i < acc.size(); i++) {
-      res += acc.get(i-1) * acc.get(i);
+    // for (int i = 1; i < acc.size(); i++) {
+      // res += acc.get(i-1) * acc.get(i);
+    for (int i = 1; i < size; i++) {
+      res += acc[i-1] * acc[i];
     }
     return res;
   }
