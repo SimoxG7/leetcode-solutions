887fd6fce033d345dee16021265095e36f1b620c
SimoxG7 <simone.gioe@studenti.unimi.it>
2024-02-20 20:53:44 +0100

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/268. Missing Number/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+  public int missingNumber(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length;
+    for (int i = 0; i < n; i++) {
+      if (nums[i] != i) {
+        return i;
+      }
+    }
+    return n;
+  }
+}
