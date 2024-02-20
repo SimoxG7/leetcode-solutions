5ac6bb1d55bcdd58124ec2ded91ced5d789efb7c
SimoxG7 <simone.gioe@studenti.unimi.it>
2024-02-20 20:56:14 +0100

    added yesterday's leetcode

---
+++ LeetCode (Dailys and from Problemset)/231. Power of Two/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+  public boolean isPowerOfTwo(int n) {
+    while (n > 0) {
+      if (n == 1) {
+        return true;
+      } else if (n % 2 == 0) {
+        n = n / 2;
+      } else {
+        return false;
+      }
+    }
+    return false;
+  }
+}
