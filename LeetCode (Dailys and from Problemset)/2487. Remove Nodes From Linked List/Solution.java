483378aecaf5d3f0f24ad911c5a8f0b18fd0a1c6
Simox <simox.gio1d@gmail.com>
2024-05-06 11:56:17 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/2487. Remove Nodes From Linked List/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode removeNodes(ListNode head) {
+    if (head == null) return null;
+    head.next = removeNodes(head.next);
+    return head.next != null && head.val < head.next.val ? head.next : head;
+  }
+}
