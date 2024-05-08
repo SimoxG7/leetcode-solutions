7da7b8919d4503e4b58c8e83555bab6a0cc590e0
Simox <simox.gio1d@gmail.com>
2024-05-08 15:54:10 +0200

    added another leetcode

---
+++ LeetCode (Dailys and from Problemset)/222. Count Complete Tree Nodes/Solution.java
@@ -0,0 +1,47 @@
+import java.util.Objects;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public int countNodes(TreeNode root) {
+    return dfs(root);
+  }
+
+  private int dfs(TreeNode root) {
+    if (Objects.isNull(root)) return 0;
+    return 1 + dfs(root.left) + dfs(root.right);
+  }
+}
