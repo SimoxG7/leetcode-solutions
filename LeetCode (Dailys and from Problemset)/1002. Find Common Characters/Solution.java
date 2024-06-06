3a7550cce30885f7e3cc22f33d08d84f9f2a6ab2
SimoxG7 <simone.gioe@studenti.unimi.it>
2024-06-06 18:31:40 +0200

    added yesterday's leetcode

--- LeetCode (Dailys and from Problemset)/1002. Find Common Characters/Solution.java
+++ LeetCode (Dailys and from Problemset)/1002. Find Common Characters/Solution.java
@@ -1,40 +1,29 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 class Solution {
   public List<String> commonChars(String[] words) {
-    List<String> res = new ArrayList<>();
-    int numWords = words.length;
-    Map<Character, Integer> mainMap = createCharMap(words[0]);
-    for (int i = 1; i < numWords; i++) {
-      Map<Character, Integer> newMap = createCharMap(words[i]);
-      for (Entry<Character, Integer> entry : newMap.entrySet()) {
-        Character key = entry.getKey();
-        if (mainMap.containsKey(key)) {
-          mainMap.put(key, Math.min(entry.getValue(), newMap.get(key)));
-        } else {
-          mainMap.remove(key);
-        }
-      }
-    }
-    for (Entry<Character, Integer> entry : mainMap.entrySet()) {
-      for (int i = 0; i < entry.getValue(); i++) {
-        String value = entry.getKey() + "";
-        res.add(value);
-      }
-    }
-    return res;
-  }
+    int[] finalFreq = new int[26];
+    Arrays.fill(finalFreq, Integer.MAX_VALUE);
 
-  Map<Character, Integer> createCharMap(String word) {
-    Map<Character, Integer> map = new HashMap<>();
-    for (int i = 0; i < word.length(); i++) {
-      char c = word.charAt(i);
-      map.put(c, map.getOrDefault(c, 0) + 1);
+    for (String word : words) {
+      int[] curFreq = new int[26];
+      for (char c : word.toCharArray()) {
+        curFreq[c - 'a']++;
+      }
+      for (int i = 0; i < 26; i++) {
+        finalFreq[i] = Math.min(finalFreq[i], curFreq[i]);
+      }
     }
-    return map;
+
+    List<String> ans = new ArrayList<>();
+    for (int i = 0; i < 26; i++) {
+      while (finalFreq[i] > 0) {
+        ans.add(String.valueOf((char) (i + 'a')));
+        finalFreq[i]--;
+      }
+    }
+    return ans;
   }
 }
