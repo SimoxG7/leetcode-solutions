f6e4406ef0e518e75df915539c3b1089d20ed64b
SimoxG7 <simone.gioe@studenti.unimi.it>
2024-08-03 10:09:34 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/1460. Make Two Arrays Equal by Reversing Subarrays/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+  public boolean canBeEqual(int[] target, int[] arr) {
+    Arrays.sort(target);
+    Arrays.sort(arr);
+    return Arrays.equals(target, arr);
+  }
+}
