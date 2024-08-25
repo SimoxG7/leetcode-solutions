ac05a21e447abbb6bbaadffc2636ad1c87b5025f
SimoxG7 <simone.gioe@studenti.unimi.it>
2024-08-25 11:15:06 +0200

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/145. Binary Tree Postorder Traversal/Solution2.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+
+class Solution {
+
+  public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+      this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+      this.val = val;
+      this.left = left;
+      this.right = right;
+    }
+  }
+
+  public List<Integer> postorderTraversal(TreeNode root) {
+    List<Integer> lst = new ArrayList<>();
+    traverse(lst, root);
+    return lst;
+  }
+
+  private void traverse(List<Integer> lst, TreeNode root) {
+    if (root != null) {
+      traverse(lst, root.left);
+      traverse(lst, root.right);
+      lst.add(root.val);
+    }
+  }
+}
