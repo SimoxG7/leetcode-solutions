1264280fc734698a8754ebd60653f3661d78c252
SimoxG7 <simone.gioe@studenti.unimi.it>
2024-09-18 18:43:47 +0200

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/179. Largest Number/Solution2.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution {
+  public String largestNumber(int[] num) {
+    if (num == null || num.length == 0)
+      return "";
+
+    String[] s_num = new String[num.length];
+    for (int i = 0; i < num.length; i++)
+      s_num[i] = String.valueOf(num[i]);
+
+    Arrays.sort(s_num, new Comparator<String>() {
+      @Override
+      public int compare(String str1, String str2) {
+        String s1 = str1 + str2;
+        String s2 = str2 + str1;
+        return s2.compareTo(s1);
+      }
+    });
+    if (s_num[0].charAt(0) == '0')
+      return "0";
+
+    StringBuilder sb = new StringBuilder();
+    for (String s : s_num)
+      sb.append(s);
+
+    return sb.toString();
+
+  }
+}
