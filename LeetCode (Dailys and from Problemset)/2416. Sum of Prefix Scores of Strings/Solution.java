392c4c01b39d07dfe27bb26b4429cce9d3b95da8
Simox <simox.gio1d@gmail.com>
2024-09-25 09:01:25 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/2416. Sum of Prefix Scores of Strings/Solution.java
@@ -0,0 +1,32 @@
+class Solution {
+
+  public class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    int count = 0;
+  }
+
+  public int[] sumPrefixScores(String[] words) {
+    TrieNode root = new TrieNode();
+    for (String word : words) {
+      TrieNode node = root;
+      for (char ch : word.toCharArray()) {
+        if (node.children[ch - 'a'] == null) {
+          node.children[ch - 'a'] = new TrieNode();
+        }
+        node = node.children[ch - 'a'];
+        node.count++;
+      }
+    }
+    int[] result = new int[words.length];
+    for (int i = 0; i < words.length; i++) {
+      TrieNode node = root;
+      int score = 0;
+      for (char ch : words[i].toCharArray()) {
+        node = node.children[ch - 'a'];
+        score += node.count;
+      }
+      result[i] = score;
+    }
+    return result;
+  }
+}
