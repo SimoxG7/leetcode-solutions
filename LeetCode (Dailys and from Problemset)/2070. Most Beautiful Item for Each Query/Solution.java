492cae59f684ec29fec49668f1a0e38fd15bdae4
Simox <simox.gio1d@gmail.com>
2024-11-12 14:20:42 +0100

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/2070. Most Beautiful Item for Each Query/Solution.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Solution {
+  public int[] maximumBeauty(int[][] items, int[] queries) {
+
+    Arrays.sort(items, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+    int currMaxBeauty = 0;
+
+    TreeMap<Integer, Integer> map = new TreeMap<>();
+    for (int[] item : items) {
+      int price = item[0];
+      int beauty = item[1];
+
+      if (currMaxBeauty >= beauty)
+        continue;
+      currMaxBeauty = beauty;
+      map.put(price, currMaxBeauty);
+
+    }
+
+    int no_queries = queries.length;
+    int[] answer = new int[no_queries];
+
+    for (int i = 0; i < no_queries; i++) {
+      Map.Entry<Integer, Integer> entry = map.floorEntry(queries[i]);
+      if (entry != null) {
+        answer[i] = entry.getValue();
+      } else {
+        answer[i] = 0;
+      }
+    }
+    return answer;
+
+  }
+}
