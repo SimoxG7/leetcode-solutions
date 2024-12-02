e338fd7c8028b4c253ea83c24775072df50a4f91
Simox <simox.gio1d@gmail.com>
2024-12-02 13:41:07 +0100

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/1455. Check If a Word Occurs As a Prefix of Any Word in a Sentence/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+  public int isPrefixOfWord(String sentence, String searchWord) {
+    String[] split = sentence.split(" ");
+    for (int i = 0; i < split.length; i++) {
+      if (split[i].startsWith(searchWord)) return i+1; 
+    }
+    return -1;
+  }
+}
