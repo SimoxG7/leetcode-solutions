edf19262f73fcc0ce536d6e3793ff1f94120c983
SimoxG7 <simone.gioe@studenti.unimi.it>
2025-03-30 19:22:03 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/763. Partition Labels/Solution.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<Integer> partitionLabels(String s) {
+    int n = s.length();
+    List<Integer> result = new ArrayList<>();
+
+    int[] lastIndex = new int[26];
+    for (int i = 0; i < n; i++) {
+      lastIndex[s.charAt(i) - 'a'] = i;
+    }
+
+    int i = 0;
+    int start = 0;
+    int end = 0;
+    while (i < n) {
+      end = Math.max(end, lastIndex[s.charAt(i) - 'a']);
+      if (i == end) {
+        result.add(end - start + 1);
+        start = end + 1;
+      }
+      i++;
+    }
+
+    return result;
+  }
+
+}
