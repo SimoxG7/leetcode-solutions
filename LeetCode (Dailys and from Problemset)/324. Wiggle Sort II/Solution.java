5d54b02e9f8386d88f06001244d5bc7cfd99a7cf
Simox <simox.gio1d@gmail.com>
2025-04-01 15:06:56 +0200

    added another leetcode

---
+++ LeetCode (Dailys and from Problemset)/324. Wiggle Sort II/Solution.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+  public void wiggleSort(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length;
+    int left = (n - 1) / 2;
+    int right = n - 1;
+    int res[] = new int[n];
+    int i = 0;
+    while (left >= 0) {
+      res[i++] = nums[left--];
+      if (i >= n)
+        break;
+      res[i++] = nums[right--];
+    }
+    for (i = 0; i < n; i++)
+      nums[i] = res[i];
+  }
+}
