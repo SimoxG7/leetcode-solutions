c386d88fea24f055eca7fed2b92f1aa36a662e6c
SimoxG7 <simone.gioe@studenti.unimi.it>
2025-05-24 11:44:46 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/2942. Find Words Containing Character/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+  public List<Integer> findWordsContaining(String[] words, char x) {
+    List<Integer> res = new ArrayList<>();
+    for (int i = 0; i < words.length; i++) {
+      for (char c : words[i].toCharArray()) {
+        if (c == x) {
+          res.add(i);
+          break;
+        }
+      }
+    }
+    return res;
+  }
+}
