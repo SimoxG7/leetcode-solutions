56752dfb9d6f6c87a355db2a015af0985d07ce7a
Simox <simox.gio1d@gmail.com>
2025-09-17 09:14:23 +0200

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/2353. Design a Food Rating System/Solution2.java
@@ -0,0 +1,62 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+class FoodRatings {
+
+  class Pair<T1, T2> {
+    T1 first;
+    T2 second;
+
+    Pair(T1 first, T2 second) {
+      this.first = first;
+      this.second = second;
+    }
+
+    T1 getKey() {
+      return first;
+    }
+
+    T2 getValue() {
+      return second;
+    }
+  }
+
+  private Map<String, Pair<String, Integer>> foodInfo = new HashMap<>();
+  private Map<String, SortedSet<Pair<Integer, String>>> sortedCuisine = new HashMap<>();
+
+  public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
+    for (int i = 0; i < foods.length; i++) {
+      String food = foods[i];
+      String cuisine = cuisines[i];
+      int rating = ratings[i];
+
+      foodInfo.put(food, new Pair<>(cuisine, rating));
+      sortedCuisine
+          .computeIfAbsent(cuisine, k -> new TreeSet<>(Comparator
+              .<Pair<Integer, String>, Integer>comparing(Pair::getKey)
+              .reversed()
+              .thenComparing(Pair::getValue)))
+          .add(new Pair<>(rating, food));
+    }
+  }
+
+  public void changeRating(String food, int newRating) {
+    Pair<String, Integer> info = foodInfo.get(food);
+    String cuisine = info.getKey();
+    int oldRating = info.getValue();
+
+    SortedSet<Pair<Integer, String>> sortedList = sortedCuisine.get(cuisine);
+    sortedList.remove(new Pair<>(oldRating, food));
+    sortedList.add(new Pair<>(newRating, food));
+
+    foodInfo.put(food, new Pair<>(cuisine, newRating));
+  }
+
+  public String highestRated(String cuisine) {
+    SortedSet<Pair<Integer, String>> sortedList = sortedCuisine.get(cuisine);
+    return sortedList.isEmpty() ? "" : sortedList.first().getValue();
+  }
+}
