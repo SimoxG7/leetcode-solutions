537d10d1613d569605fed6bb13021210b88990b7
SimoxG7 <simone.gioe@studenti.unimi.it>
2025-09-27 23:11:49 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/812. Largest Triangle Area/Solution.java
@@ -0,0 +1,19 @@
+class Solution {
+  public double largestTriangleArea(int[][] points) {
+    int n = points.length;
+    double ans = 0.0;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        for (int k = j + 1; k < n; k++) {
+          double area = Math.abs(points[i][0] * (points[j][1] - points[k][1]) +
+              points[j][0] * (points[k][1] - points[i][1]) +
+              points[k][0] * (points[i][1] - points[j][1])) / 2.0;
+          ans = Math.max(ans, area);
+        }
+      }
+    }
+
+    return ans;
+  }
+}
