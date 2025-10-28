2809549f86f604979f09d340ceeb2e1ccd809406
Simox <simox.gio1d@gmail.com>
2025-10-28 10:33:08 +0100

    added today's leetcode

--- LeetCode (Dailys and from Problemset)/3354. Make Array Elements Equal to Zero/Solution.java
+++ LeetCode (Dailys and from Problemset)/3354. Make Array Elements Equal to Zero/Solution.java
@@ -20,3 +20,36 @@ class Solution {
     return ans;
   }
 }
+
+// class Solution {
+//   public int countValidSelections(int[] nums) {
+//     int n = nums.length;
+//     if (n == 1) return 2;
+//     int left = 0, right = 0;
+//     int leftIndex = 0, rightIndex = nums.length - 1;
+//     while (leftIndex < rightIndex) {
+//       if (left <= right) {
+//         left += nums[leftIndex++];
+//       } else {
+//         right += nums[rightIndex--];
+//       }
+//     }
+//     rightIndex++;
+//     if (left == 0) return n * 2;
+//     int res = 0, zeroesEncountered = 0;
+//     while (nums[leftIndex] == 0 && leftIndex > 0) {
+//       leftIndex--;
+//       zeroesEncountered++;
+//     }
+//     while (nums[rightIndex] == 0 && rightIndex < n) {
+//       rightIndex++;
+//       zeroesEncountered++;
+//     }
+//     if (left == right) {
+//       res = zeroesEncountered * 2;
+//     } else if (Math.abs(left - right) == 1) {
+//       res = zeroesEncountered;
+//     }
+//     return res;
+//   }
+// }
