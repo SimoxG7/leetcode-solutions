1d0dcdbb8c50adf8f04d15a4e39a85439c575c65
Simox <simox.gio1d@gmail.com>
2026-02-06 09:21:18 +0100

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/3634. Minimum Removals to Balance Array/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+  public int minRemoval(int[] nums, int k) {
+    Arrays.sort(nums);
+    int i = 0;
+    int maxLen = 0;
+
+    for (int j = 0; j < nums.length; j++) {
+      while ((long) nums[j] > (long) nums[i] * k) {
+        i++;
+      }
+      maxLen = Math.max(maxLen, j - i + 1);
+    }
+
+    return nums.length - maxLen;
+  }
+}
