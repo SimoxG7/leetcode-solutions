2480adbe67cb8740d3ed0fd936c3dfa38bee2284
Simox <simox.gio1d@gmail.com>
2026-04-01 08:58:58 +0200

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/2751. Robot Collisions/Solution2.java
@@ -0,0 +1,51 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+class Solution {
+  public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+    int n = positions.length;
+    Integer[] indices = new Integer[n];
+    for (int i = 0; i < n; i++) {
+      indices[i] = i;
+    }
+
+    Arrays.sort(indices, (a, b) -> positions[a] - positions[b]);
+
+    Deque<Integer> stack = new ArrayDeque<>();
+    boolean[] eliminated = new boolean[n];
+
+    for (int index : indices) {
+      if (directions.charAt(index) == 'L') {
+        while (!stack.isEmpty() && !eliminated[index]) {
+          int top = stack.peek();
+          if (healths[index] < healths[top]) {
+            eliminated[index] = true;
+            healths[top]--;
+          } else if (healths[index] > healths[top]) {
+            stack.pop();
+            eliminated[top] = true;
+            healths[index]--;
+          } else {
+            stack.pop();
+            eliminated[top] = true;
+            eliminated[index] = true;
+            break;
+          }
+        }
+      } else {
+        stack.push(index);
+      }
+    }
+
+    List<Integer> result = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      if (!eliminated[i]) {
+        result.add(healths[i]);
+      }
+    }
+    return result;
+  }
+}
