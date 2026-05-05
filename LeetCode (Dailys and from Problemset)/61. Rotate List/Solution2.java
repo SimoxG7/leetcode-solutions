3094f75d7e291aef9d0edacd20d24c023e9619e8
Simox <simox.gio1d@gmail.com>
2026-05-05 18:19:43 +0200

    added today's leetcode (dup)

---
+++ LeetCode (Dailys and from Problemset)/61. Rotate List/Solution2.java
@@ -0,0 +1,43 @@
+class Solution {
+
+  public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+      this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+      this.val = val;
+      this.next = next;
+    }
+  }
+
+  public ListNode rotateRight(ListNode head, int k) {
+    if (head == null || head.next == null || k == 0) {
+      return head;
+    }
+
+    ListNode curr = head;
+    int len = 1;
+    
+    while (curr.next != null) {
+      len++;
+      curr = curr.next;
+    }
+
+    curr.next = head;
+    k = k % len;
+    k = len - k;
+    while (k-- > 0) {
+      curr = curr.next;
+    }
+    head = curr.next;
+    curr.next = null;
+    return head;
+  }
+}
