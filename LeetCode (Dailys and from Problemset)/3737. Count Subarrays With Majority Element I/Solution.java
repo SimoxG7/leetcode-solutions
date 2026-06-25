bb3a5c357623b7f3af1d99800445d5a4e82e6211
Simox <simox.gio1d@gmail.com>
2026-06-25 11:31:40 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/3737. Count Subarrays With Majority Element I/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int countMajoritySubarrays(int[] nums, int target) {
+    int len = nums.length, res = 0;
+    int[] arr = new int[len];
+    for (int i = 0; i < len; i++) {
+      arr[i] = (nums[i] == target) ? 1 : -1;
+    }
+    for (int i = 0; i < len; i++) {
+      int sum = 0;
+      for (int j = i; j < len; j++) {
+        sum += arr[j];
+        if (sum > 0)
+          res++;
+      }
+    }
+    return res;
+  }
+}
