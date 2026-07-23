31b4f53e1660ed51ed45fa2b546f5135fd479313
Simox <simox.gio1d@gmail.com>
2026-07-23 14:28:58 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/3513. Number of Unique XOR Triplets I/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+  public int uniqueXorTriplets(int[] nums) {
+    int n = nums.length;
+    if (n <= 2) return n;
+
+    int res = 1;
+    while (res <= n) {
+      res <<= 1;
+    }
+    return res;
+  }
+}
