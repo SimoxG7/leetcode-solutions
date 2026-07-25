b8001644979a6e71de57f732ae30e40c56a0b72c
Simox <simox.gio1d@gmail.com>
2026-07-25 13:31:23 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/3536. Maximum Product of Two Digits/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+	public int maxProduct(int n) {
+		int first = 0, second = 0;
+		while (n > 0) {
+			int digit = n % 10;
+			if (digit > first) {
+				second = first;
+				first = digit;
+			} else if (digit > second) second = digit;
+			n /= 10;
+		}
+		return first * second;
+	}
+}
