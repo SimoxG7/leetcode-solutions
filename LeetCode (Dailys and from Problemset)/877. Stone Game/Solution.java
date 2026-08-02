cd5d29acebb557a82d6087d20e2d44fa2d8af42d
Simox <simox.gio1d@gmail.com>
2026-08-02 11:35:30 +0200

    added today's leetcode

---
+++ LeetCode (Dailys and from Problemset)/877. Stone Game/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+  public boolean stoneGame(int[] piles) {
+    return true;
+    // int sumEven = 0, sumOdd = 0;
+    // boolean isEven = true;
+    // for (int i = 0; i < piles.length; i++) {
+    //   if (isEven) sumEven += piles[i];
+    //   else sumOdd += piles[i];
+    //   isEven = !isEven;
+    // }
+  }
+}
