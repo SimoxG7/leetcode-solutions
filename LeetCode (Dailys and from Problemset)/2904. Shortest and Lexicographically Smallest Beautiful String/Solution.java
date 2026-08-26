8d92e31cd5ba30db0b148bff7edd96f40c72a0bc
Simox <simox.gio1d@gmail.com>
2026-08-26 12:25:04 +0200

    improved solution writing

--- LeetCode (Dailys and from Problemset)/2904. Shortest and Lexicographically Smallest Beautiful String/Solution.java
+++ LeetCode (Dailys and from Problemset)/2904. Shortest and Lexicographically Smallest Beautiful String/Solution.java
@@ -1,45 +1,38 @@
 class Solution {
   public String shortestBeautifulSubstring(String s, int k) {
     int oneCounter = 0;
-    char[] sCharArray = s.toCharArray();
+    char[] stringCharArray = s.toCharArray();
 
     String min = "";
     int minLenght = Integer.MAX_VALUE;
-    int sLength = sCharArray.length;
-    int firstOneIndex = Integer.MAX_VALUE;
     int left = 0, right = 0;
-    
-    for (int i = 0; i < sLength; i++) {
-      if (sCharArray[i] == '1') {
-        firstOneIndex = Math.min(firstOneIndex, i);
-        oneCounter++;
-        if (oneCounter == k) {
-          left = firstOneIndex;
-          right = i + 1;
-          min = s.substring(firstOneIndex, right);
-          minLenght = min.length();
-          break;
-        }
-      }
-    }
 
-    if (minLenght == Integer.MAX_VALUE) return "";
-
-    while (right < sLength) {
-      if (sCharArray[right] != '1') {
-        right++;
+    while (right < stringCharArray.length) {
+      if (stringCharArray[right++] != '1')
         continue;
-      }
 
-      while (sCharArray[++left] != '1') {}
-      right++;
-      int newLength = right - left;
-      if (newLength < minLenght) {
-        min = s.substring(left, right);
-        minLenght = min.length();
-      } else if (newLength == minLenght) {
+      oneCounter++;
+
+      while (oneCounter > k)
+        if (stringCharArray[left++] == '1')
+          oneCounter--;
+
+      while (stringCharArray[left] != '1')
+        left++;
+
+      if (oneCounter == k) {
+        int newLength = right - left;
+        if (newLength > minLenght)
+          continue;
+
         String newMinCandidate = s.substring(left, right);
-        min = min.compareTo(newMinCandidate) <= 0 ? min : newMinCandidate;
+
+        if (newLength < minLenght) {
+          min = newMinCandidate;
+          minLenght = min.length();
+        } else
+          min = min.compareTo(newMinCandidate) <= 0 ? min : newMinCandidate;
+
       }
     }
 
